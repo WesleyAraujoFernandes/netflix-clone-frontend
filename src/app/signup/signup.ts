e9e02daf5f0d55ee396f6552e71b3e966a296fe6
wesleyfernandesprojetofaculdade <wesley@viaapia.com.br>
2026-05-28 12:03:33 -0300

    signup and email verify

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotificationService } from '../shared/services/notification-service';
+import { ErrorHandlerService } from '../shared/services/error-handler-service';
+import { AuthService } from '../shared/services/auth-service';
 
 @Component({
   selector: 'app-signup',
@@ -7,5 +12,24 @@ import { Component } from '@angular/core';
   styleUrl: './signup.css',
 })
 export class Signup {
+  hidePassword = true;
+  hideConfirmPassword = true;
+  signupForm!: FormGroup;
+  loading = false;
 
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private notification: NotificationService,
+    private errorHandlerService: ErrorHandlerService
+  ) {
+    this.signupForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, this.authService.passwordMatchValidator('password')]], // 'password' = nome do campo que eu quero comparar
+    })
+  }
 }
